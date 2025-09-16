64c96e94cd78553164efdfb02ea5a4bab80c46a9
Luis Henrique <lhvv@ic.ufal.br>
2025-09-16 18:44:06 -0300

    Update

--- davies_bouldin.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DAVIES_BOULDIN_HPP
-#define DAVIES_BOULDIN_HPP
-
-#include <vector>
-
-double daviesBouldinIndex(const std::vector<std::vector<double>>& data,
-                          const std::vector<int>& labels, int k);
-
-#endif
