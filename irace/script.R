ecee2868737da1b8bbc133485b13a8b40470cd09
Luis Henrique <lhvv@ic.ufal.br>
2026-01-22 11:00:48 -0300

    Update

--- irace/script.R
+++ irace/script.R
@@ -1,6 +1,12 @@
-setwd("C:/workingdir/irace")
+# Setup
+setwd("C:/Users/Luis Henrique/Documents/Faculdade/Orion/ct-transportes/implementacao_romulo/hybrid_cpp/irace")
 library(irace)
+library(iraceplot)
 
+# Irace
 scenario <- readScenario(filename = "scenario.txt",
                          scenario = defaultScenario())
 irace_main(scenario = scenario)
+
+# Read log file
+report("logs/irace.log")
